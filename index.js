3595f88ae8fe1cfc0475bed1e7aaf39aaf1e3e92
vinod <vinodraju110@gmail.com>
2025-01-15 23:21:55 +0530

    dom project1

---
+++ index.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("div");
+let text = document.getElementById("text");
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
+
